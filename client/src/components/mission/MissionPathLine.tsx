86fde1270da4d523ddf6bb43325c64cb2b163f77
Anushka <lavianu2004@gmail.com>
2026-07-09 09:58:42 +0530

    updated mission path line

--- client/src/components/mission/MissionPathLine.tsx
+++ client/src/components/mission/MissionPathLine.tsx
@@ -14,58 +14,28 @@ export default function MissionPathLine() {
       viewBox="0 0 100 100"
       preserveAspectRatio="none"
     >
-
-      {/* Glow underneath */}
-
-      <path
+      <motion.path
         d="
-        M 35 85
-        C 55 80, 65 75, 55 65
-        C 45 55, 35 55, 45 45
-        C 60 35, 70 35, 60 25
-        C 50 15, 45 10, 55 5
+          M30 82
+          C45 78 50 72 58 70
+          C70 66 55 56 38 55
+          C25 52 45 42 65 40
+          C80 37 60 28 45 25
         "
         fill="none"
         stroke="#22d3ee"
-        strokeWidth="1.5"
-        opacity="0.25"
-        filter="blur(2px)"
-      />
-
-
-      {/* Main animated path */}
-
-      <motion.path
-        d="
-        M 35 85
-        C 55 80, 65 75, 55 65
-        C 45 55, 35 55, 45 45
-        C 60 35, 70 35, 60 25
-        C 50 15, 45 10, 55 5
-        "
-
-        fill="none"
-
-        stroke="#67e8f9"
-
-        strokeWidth="0.6"
-
+        strokeWidth="0.5"
         strokeLinecap="round"
-
-        strokeDasharray="3 3"
-
+        strokeDasharray="2 2"
         animate={{
           strokeDashoffset: [20, 0],
         }}
-
         transition={{
-          duration: 2,
+          duration: 3,
           repeat: Infinity,
           ease: "linear",
         }}
       />
-
-
     </svg>
   );
 }
